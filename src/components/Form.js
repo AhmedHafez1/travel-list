322a006a309558267a469f23ef201bb521ab1673
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-09-27 14:26:32 +0300

    Form submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className='add-form'>
-      <h3>What do you need for trip?</h3>
-    </div>
-  )
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for trip 😁?</h3>
+      <select>
+        {Array.from({ length: 15 }, (_, i) => i + 1).map((x) => (
+          <option value={x} key={x}>
+            {x}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
+  );
 }
