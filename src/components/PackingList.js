9f19cb19c48213675e09e434e8a81897a43633b9
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-09-27 08:56:47 +0300

    packingList

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+
+const packingList = [
+  { name: "Passport", quantity: 1, packed: false },
+  { name: "Bag", quantity: 2, packed: false },
+  { name: "Papers", quantity: 5, packed: true },
+];
 
 export default function PackingList() {
   return (
-    <div className='list'>
-      List
+    <div className="list">
+      <ul>
+        {packingList.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
     </div>
-  )
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={{ textDecoration: item.packed ? "line-through" : "" }}>
+        {item.quantity} {item.name}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
